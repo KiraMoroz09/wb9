14b85df0123524fdfd1a6a577c47ae7c799d2f39
kiraMoroz09 <pivasikboom673@gmail.com>
2023-01-22 20:06:44 +0300

    death

---
+++ java.js
@@ -0,0 +1,89 @@
+// const age = 18
+
+// if (age > 18) {
+//     console.log('true');
+// } else if (age < 18 ) {
+//     console.log('false');
+// } else {
+//     console.log('= 18');
+// }
+
+// console.log(1 =='1');
+
+// const n = 'zaza'
+// if (n !== 'zaza') {
+//       console.log('yes');
+// }
+
+// console.log(24836 % 25);
+
+// const userName = 'mike'
+
+// switch (userName) {
+//     case 'mike':
+//         console.log('hello , mike');
+//         break;
+//     case 'mary':
+//         console.log('hello , mary');
+//         break;
+//     case 'nice':
+//         console.log('hello , nice');
+//         break;
+
+//     default:
+//         console.log('who you are ?');
+//     break;
+// }
+
+// const sex = 'man' 
+// const age = 30 
+// const hairCorol = 'blue'
+
+// if (sex === 'man' || age >= 30 && haitColor !== 'blue') {
+//     console.log('u r welcome');
+// } else {
+//     console.log('sorry');
+// }
+
+// let i = 0
+     
+// while (1>5) {
+//     console.log(i);
+//     i++
+//     if (i == 10){
+//          break;
+//     }
+   
+// }
+
+
+// let i = 0
+     
+// do {
+//    console.log(i);
+//    i++
+// } while (1 < 5) {
+
+// } 
+    
+
+// for (let i = 0 ; i < 10 ; i+=2) {
+//     console.log(i);
+// }
+
+// const arr = [ 0 ,11 , 6 , 10 , 46 , 23 , 222 , 676 ]
+// for (let i = 0 ; i < arr.length; i++) {
+//     if (arr[i]  % 2 === 0 && i%2 !==0)  {
+//           console.log(arr[i]);
+//     }        
+// }
+
+const arr = [ 0 ,11 , 6 , 10 , 46 , 23 , 222 , 676 ]
+
+let sum = 0
+
+for (let i = 0 ; i < arr.length ; i++) {
+   sum = sum + arr[i]
+   console.log(sum);
+}
+
